b5e39cd9ba00c8ea32441b6a774f1e95335bb145
Owaish <owaish3301@gmail.com>
2026-02-24 07:32:48 +0530

    adding post models

--- blog/posts/models.py
+++ blog/posts/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=250)
+    slug = models.SlugField(unique=True, max_length=250)
+    body = models.TextField()
+    # author = models.ForeignKey(User, on_delete=models.CASCADE)
+    published_at = models.DateTimeField(default=timezone.now)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ["-published_at"]
+        indexes = [models.Index(fields=["-pulished_at"]), ]
+
+    def __str__(self):
+        return self.title
