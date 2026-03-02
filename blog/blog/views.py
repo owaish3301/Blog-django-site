f86504e62a52fcaae5557aef4040ccabb9c15a41
Owaish <owaish3301@gmail.com>
2026-03-02 17:30:58 +0530

    enhance landing page with latest posts and topics grid

--- blog/blog/views.py
+++ blog/blog/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from posts.models import Post
+
 
 def landing_page(request):
-    return render(request, 'landing_page/index.html')
+    latest_posts = Post.objects.filter(status=Post.Status.PUBLISHED).order_by('-published_at')[:3]
+    return render(request, 'landing_page/index.html', {'latest_posts': latest_posts})
