9ef7a8b721d588e6dc2448a376b7b9236c0c1f3b
Owaish <owaish3301@gmail.com>
2026-03-02 07:29:27 +0530

    restructure

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'posts.apps.PostsConfig',
+    'landing_page.apps.LandingPageConfig',
     'django_browser_reload',
 ]
 
