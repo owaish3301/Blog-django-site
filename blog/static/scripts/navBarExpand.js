ee4d2afc3fe79ae630adfae21c0001c268810dc8
Owaish <owaish3301@gmail.com>
2026-03-29 20:07:24 +0530

    fixed micro animations across landing page and blogs page

--- blog/static/scripts/navBarExpand.js
+++ blog/static/scripts/navBarExpand.js
@@ -2,20 +2,20 @@ const navbar = document.querySelector("nav");
 const navDropDown = document.querySelector(".nav-dropdown");
 const navDropDownIcon = document.querySelector(".lucide-chevron-down");
 
-function showDropDown(){
+function showDropDown() {
     const isDropDownActive = navbar.classList.contains("hidden");
-    if(isDropDownActive){
+    if (isDropDownActive) {
         navbar.classList.remove("hidden");
         navDropDownIcon.style.transform = "rotate(180deg)"
     }
-    else{
+    else {
         navbar.classList.add("hidden");
         navDropDownIcon.style.transform = "rotate(0deg)"
     }
 }
 
 
-navDropDown.addEventListener('click',showDropDown)
+navDropDown.addEventListener('click', showDropDown)
 
 // Highlight active nav link
 const navLinks = document.querySelectorAll("a.nav-link");
