17464698bd5192ad9d39365f8c6008300d7f1936
Owaish <owaish3301@gmail.com>
2026-03-30 00:32:06 +0530

    updated settings

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ce1@)owr*5xi6(gw+0w0@zjz_-r042u==b_6)fjp$2w#-14k)^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
