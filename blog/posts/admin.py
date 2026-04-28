b3f85f9857a1d6d1a2084cb28b77e63699d164db
owaish <owaish3301@gmail.com>
2026-04-28 08:05:17 +0530

    added category to admin and update landing page layout

--- blog/posts/admin.py
+++ blog/posts/admin.py
@@ -2,17 +2,18 @@ from django.contrib import admin
 
 # Register your models here.
 from .models import Post
+from .models import Category
 
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
-    list_display = ['title', 'slug', 'author', 'published_at', 'status' ]
-    list_filter = [ 'status', 'created_at', 'published_at', 'updated_at', 'author' ]
-    search_fields = ['title', 'slug']
-    prepopulated_fields = {'slug':('title',)}
-    raw_id_fields = ['author']
-    date_hierarchy = 'published_at'
-    ordering = ['status', 'published_at']
+    list_display = ["title", "slug", "author", "published_at", "status"]
+    list_filter = ["status", "created_at", "published_at", "updated_at", "author"]
+    search_fields = ["title", "slug"]
+    prepopulated_fields = {"slug": ("title",)}
+    raw_id_fields = ["author"]
+    date_hierarchy = "published_at"
+    ordering = ["status", "published_at"]
     show_facets = admin.ShowFacets.ALWAYS
 
     class Media:
@@ -21,3 +22,9 @@ class PostAdmin(admin.ModelAdmin):
             "https://cdn.jsdelivr.net/npm/marked/marked.min.js",
             "posts/scripts/admin_markdown.js",
         )
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    pass
+
